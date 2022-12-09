f2ae71cdd162707885d9941a9d72ca46f7483b53
vismate <visnyarmate@gmail.com>
2022-12-09 02:06:34 +0100

    implemented dot rendering for DFSOutput, modified dfs example to save result to .dot file

--- examples/dfs.rs
+++ examples/dfs.rs
@@ -1,7 +1,12 @@
 use algo_graph::{dfs::dfs, graph::Graph};
+use std::fs::File;
 
 fn main() {
     let g = Graph::from_adjacency_list([vec![(1, 1), (2, 1)], vec![(3, 1)], vec![(3, 1)], vec![]]);
 
-    println!("{:?}", dfs(&g));
+    let dfs_output = dfs(&g);
+    println!("{dfs_output:?}");
+
+    let mut f = File::create("dfs_output.dot").expect("could not create file");
+    dot::render(&dfs_output, &mut f).expect("could not save output to file");
 }
--- src/dot_impl.rs
+++ src/dot_impl.rs
@@ -1,8 +1,42 @@
 use crate::{
     bfs::BFSOutput,
+    dfs::{DFSOutput, EdgeKind},
     graph::{Edge, Graph, Vertex},
 };
 use dot::{GraphWalk, Id, LabelText, Labeller};
+use infinitable::Infinitable;
+
+fn infinitable_to_string<T: std::fmt::Display>(v: &Infinitable<T>) -> String {
+    match v {
+        Infinitable::Finite(n) => n.to_string(),
+        Infinitable::Infinity => "inf".into(),
+        Infinitable::NegativeInfinity => "-inf".into(),
+    }
+}
+
+fn node_id<'a>(n: Vertex) -> Id<'a> {
+    Id::new(format!("N{n}")).unwrap()
+}
+
+fn node_label_char(n: Vertex) -> char {
+    ('a'..='z')
+        .nth(n)
+        .expect("cannot label more than 26 nodes at the moment")
+}
+
+fn node_label_with_value<'a, T: std::fmt::Display>(
+    n: Vertex,
+    value: &Infinitable<T>,
+) -> LabelText<'a> {
+    LabelText::LabelStr(format!("{} : {}", node_label_char(n), infinitable_to_string(value)).into())
+}
+
+fn edges_from_pi<'a, const N: usize>(pi: &[Option<Vertex>; N]) -> dot::Edges<'a, Edge> {
+    pi.iter()
+        .enumerate()
+        .filter_map(|(v, u)| u.map(|u| Edge { u, v, w: 0 }))
+        .collect()
+}
 
 impl<'a, const N: usize> Labeller<'a, Vertex, Edge> for Graph<N> {
     fn graph_id(&'a self) -> Id<'a> {
@@ -10,18 +44,11 @@ impl<'a, const N: usize> Labeller<'a, Vertex, Edge> for Graph<N> {
     }
 
     fn node_id(&'a self, n: &Vertex) -> Id<'a> {
-        Id::new(format!("N{n}")).unwrap()
+        node_id(*n)
     }
 
     fn node_label<'b>(&'b self, n: &Vertex) -> LabelText<'b> {
-        LabelText::LabelStr(
-            String::from(
-                ('a'..='z')
-                    .nth(*n)
-                    .expect("cannot label more than 26 nodes at the moment"),
-            )
-            .into(),
-        )
+        LabelText::LabelStr(node_label_char(*n).to_string().into())
     }
 
     fn edge_label<'b>(&'b self, e: &Edge) -> LabelText<'b> {
@@ -50,20 +77,11 @@ impl<'a, const N: usize> Labeller<'a, Vertex, Edge> for BFSOutput<N> {
     }
 
     fn node_id(&'a self, n: &Vertex) -> Id<'a> {
-        Id::new(format!("N{n}")).unwrap()
+        node_id(*n)
     }
 
     fn node_label<'b>(&'b self, n: &Vertex) -> LabelText<'b> {
-        LabelText::LabelStr(
-            format!(
-                "{}:{}",
-                ('a'..='z')
-                    .nth(*n)
-                    .expect("cannot label more than 26 nodes at the moment"),
-                self.d[*n].finite().map_or("inf".into(), |v| v.to_string())
-            )
-            .into(),
-        )
+        node_label_with_value(*n, &self.d[*n])
     }
 }
 
@@ -73,11 +91,7 @@ impl<'a, const N: usize> GraphWalk<'a, Vertex, Edge> for BFSOutput<N> {
     }
 
     fn edges(&'a self) -> dot::Edges<'a, Edge> {
-        self.pi
-            .iter()
-            .enumerate()
-            .filter_map(|(v, u)| u.map(|u| Edge { u, v, w: 0 }))
-            .collect()
+        edges_from_pi(&self.pi)
     }
 
     fn source(&'a self, e: &Edge) -> Vertex {
@@ -86,5 +100,48 @@ impl<'a, const N: usize> GraphWalk<'a, Vertex, Edge> for BFSOutput<N> {
 
     fn target(&self, e: &Edge) -> Vertex {
         e.v
+    }
+}
+
+impl<'a, const N: usize> Labeller<'a, Vertex, (Edge, EdgeKind)> for DFSOutput<N> {
+    fn graph_id(&'a self) -> Id<'a> {
+        Id::new("dfs_output_graph").unwrap()
+    }
+
+    fn node_id(&'a self, n: &Vertex) -> Id<'a> {
+        node_id(*n)
+    }
+
+    fn node_label<'b>(&'b self, n: &Vertex) -> LabelText<'b> {
+        LabelText::LabelStr(
+            format!(
+                "{} : {}/{}",
+                node_label_char(*n),
+                infinitable_to_string(&self.d[*n]),
+                infinitable_to_string(&self.f[*n]),
+            )
+            .into(),
+        )
+    }
+    fn edge_label<'b>(&'b self, e: &(Edge, EdgeKind)) -> LabelText<'b> {
+        LabelText::LabelStr(format!("{:?} : {}", e.1, e.0.w).into())
+    }
+}
+
+impl<'a, const N: usize> GraphWalk<'a, Vertex, (Edge, EdgeKind)> for DFSOutput<N> {
+    fn nodes(&self) -> dot::Nodes<'a, Vertex> {
+        (0..N).collect()
+    }
+
+    fn edges(&'a self) -> dot::Edges<'a, (Edge, EdgeKind)> {
+        self.classified_edges.as_slice().into()
+    }
+
+    fn source(&'a self, e: &(Edge, EdgeKind)) -> Vertex {
+        e.0.u
+    }
+
+    fn target(&'a self, e: &(Edge, EdgeKind)) -> Vertex {
+        e.0.v
     }
 }
