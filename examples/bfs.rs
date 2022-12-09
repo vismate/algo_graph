98b924c8e1cba6913a6438b950af940146ebf25e
vismate <visnyarmate@gmail.com>
2022-12-09 01:13:10 +0100

    implemented dot rendering for BFSOutput, modified bfs example to save result to .dot file

--- examples/bfs.rs
+++ examples/bfs.rs
@@ -1,4 +1,5 @@
 use algo_graph::{bfs::bfs, graph::Graph};
+use std::fs::File;
 
 fn main() {
     let g = Graph::from_adjacency_list([
@@ -10,5 +11,9 @@ fn main() {
         vec![(2, 1), (4, 1)],
     ]);
 
-    println!("{:?}", bfs(&g, 0));
+    let bfs_output = bfs(&g, 0);
+    println!("{bfs_output:?}");
+
+    let mut f = File::create("bfs_output.dot").expect("could not create file");
+    dot::render(&bfs_output, &mut f).expect("could not save output to file");
 }
--- src/dot_impl.rs
+++ src/dot_impl.rs
@@ -1,4 +1,7 @@
-use crate::graph::{Edge, Graph, Vertex};
+use crate::{
+    bfs::BFSOutput,
+    graph::{Edge, Graph, Vertex},
+};
 use dot::{GraphWalk, Id, LabelText, Labeller};
 
 impl<'a, const N: usize> Labeller<'a, Vertex, Edge> for Graph<N> {
@@ -36,6 +39,51 @@ impl<'a, const N: usize> GraphWalk<'a, Vertex, Edge> for Graph<N> {
     fn source(&self, e: &Edge) -> Vertex {
         e.u
     }
+    fn target(&self, e: &Edge) -> Vertex {
+        e.v
+    }
+}
+
+impl<'a, const N: usize> Labeller<'a, Vertex, Edge> for BFSOutput<N> {
+    fn graph_id(&'a self) -> Id<'a> {
+        Id::new("bfs_output_graph").unwrap()
+    }
+
+    fn node_id(&'a self, n: &Vertex) -> Id<'a> {
+        Id::new(format!("N{n}")).unwrap()
+    }
+
+    fn node_label<'b>(&'b self, n: &Vertex) -> LabelText<'b> {
+        LabelText::LabelStr(
+            format!(
+                "{}:{}",
+                ('a'..='z')
+                    .nth(*n)
+                    .expect("cannot label more than 26 nodes at the moment"),
+                self.d[*n].finite().map_or("inf".into(), |v| v.to_string())
+            )
+            .into(),
+        )
+    }
+}
+
+impl<'a, const N: usize> GraphWalk<'a, Vertex, Edge> for BFSOutput<N> {
+    fn nodes(&self) -> dot::Nodes<'a, Vertex> {
+        (0..N).collect()
+    }
+
+    fn edges(&'a self) -> dot::Edges<'a, Edge> {
+        self.pi
+            .iter()
+            .enumerate()
+            .filter_map(|(v, u)| u.map(|u| Edge { u, v, w: 0 }))
+            .collect()
+    }
+
+    fn source(&'a self, e: &Edge) -> Vertex {
+        e.u
+    }
+
     fn target(&self, e: &Edge) -> Vertex {
         e.v
     }
